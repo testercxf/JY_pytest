0e9e108cad60c075f76f5d49b95d3a63bcbc89db
testercxf <2891867255@qq.com>
2026-01-09 17:04:12 +0800

    新增

--- testcases/test_admin.py
+++ testcases/test_admin.py
@@ -19,9 +19,10 @@ def test_admin_data():
         "name": name,
         "number": str(number),
         "deptid": deptId,
-        "project_no": project_no
+        "project_no": project_no,
         # 接口响应数据共享
-        
+        "projectId":str(None),
+        "project_name": None
         
     }
 
@@ -32,18 +33,6 @@ class Testadmin:
     def test_get_straight_project_dept_operations_tree(self,args,test_admin_data):
         method = args["request"]["method"]
         url = test_admin_data["host"]+args["request"]["url"]
-        data = args["request"]["data"]
-        SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
-    
-    @allure.story("社区直饮水项目列表")
-    @pytest.mark.parametrize("args",Getyaml().read_yaml_file("admin_data.yaml","get_straight_project_page"))
-    def test_get_straight_project_page(self,args,test_admin_data):
-        method = args["request"]["method"]
-        url = (
-            test_admin_data["host"]
-            + args["request"]["url"]
-            + "deptId={}&current=1&size=10&descs=&ascs=".format(test_admin_data["deptid"])
-        )
         data = args["request"]["data"]
         SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
     
@@ -60,3 +49,48 @@ class Testadmin:
         data["projectNo"] = test_admin_data["project_no"]
         data["projectName"] = f"直饮水{test_admin_data['project_no']}"
         SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
+    
+    @allure.story("社区直饮水项目列表")
+    @pytest.mark.parametrize("args",Getyaml().read_yaml_file("admin_data.yaml","get_straight_project_page"))
+    def test_get_straight_project_page(self,args,test_admin_data):
+        method = args["request"]["method"]
+        url = (
+            test_admin_data["host"]
+            + args["request"]["url"]
+            + "deptId={}&current=1&size=10&descs=&ascs=".format(test_admin_data["deptid"])
+        )
+        data = args["request"]["data"]
+        res = SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
+        test_admin_data["projectId"] = res.json()["data"]["records"][0]["id"]
+        test_admin_data["project_name"] = res.json()["data"]["records"][0]["projectName"]
+
+    
+    @allure.story("搜索项目名称")
+    @pytest.mark.parametrize("args",Getyaml().read_yaml_file("admin_data.yaml","search_project"))
+    def test_search_project(self,args,test_admin_data):
+        method = args["request"]["method"]
+        url = (test_admin_data["host"]+args["request"]["url"]+
+        "projectType=community_water&deptId={0}&projectName={1}&current=1&size=10&descs=&ascs="
+        .format(test_admin_data["deptid"], test_admin_data["project_name"])
+        )
+        data = args["request"]["data"]
+        SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
+        
+    @allure.story("查看项目详情")
+    @pytest.mark.parametrize("args",Getyaml().read_yaml_file("admin_data.yaml","get_project_details"))
+    def test_get_project_details(self,args,test_admin_data):
+        method = args["request"]["method"]
+        url = test_admin_data["host"]+args["request"]["url"]+test_admin_data["projectId"]
+        data = args["request"]["data"]
+        SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
+    
+    @allure.story("修改项目信息")
+    @pytest.mark.parametrize("args",Getyaml().read_yaml_file("admin_data.yaml","update_project_info"))
+    def test_update_project_info(self,args,test_admin_data):
+        method = args["request"]["method"]
+        url = test_admin_data["host"]+args["request"]["url"]
+        data = args["request"]["data"]
+        data["id"] = test_admin_data["projectId"]
+        data["deptId"] = test_admin_data["deptid"]
+        SendRequest.send_requests(method, url,data, headers={"Authorization": test_admin_data["Token"]})
+        
